ac9e999f7bf71d3f1878cad44c114333c8e26c48
Raya Anderson <rayazand@gmail.com>
2020-02-26 19:32:05 -0800

    adding migrations to sqlite

--- sockmanager/sockmanager/settings.py
+++ sockmanager/sockmanager/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'sockit',
-    'rest-framework'
+'rest_framework',
+'django.contrib.contenttypes',
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'sockit',
 ]
 
 MIDDLEWARE = [
